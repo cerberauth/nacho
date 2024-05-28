77ee5427b1a6ed1ed809bb359b90ac24e2de467a
Emmanuel Gautier <git@emmanuelgautier.fr>
2024-05-28 11:58:04 +0200

    fix: plausible integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,14 +33,6 @@ export default function RootLayout({
           operatingSystem='All'
         />
 
-        <PlausibleProvider
-          domain="nacho.cerberauth.com"
-          customDomain="https://a.cerberauth.com"
-          selfHosted={true}
-          trackOutboundLinks={true}
-          taggedEvents={true}
-        />
-
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
@@ -51,9 +43,17 @@ export default function RootLayout({
       </Head>
 
       <body className={inter.className}>
-        <div className="flex min-h-screen w-full flex-col bg-muted/40">
-          {children}
-        </div>
+        <PlausibleProvider
+          domain="nacho.cerberauth.com"
+          customDomain="https://a.cerberauth.com"
+          selfHosted={true}
+          trackOutboundLinks={true}
+          taggedEvents={true}
+        >
+          <div className="flex min-h-screen w-full flex-col bg-muted/40">
+            {children}
+          </div>
+        </PlausibleProvider>
       </body>
     </html>
   )
