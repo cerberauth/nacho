40713cb1932ad40aef2ee7c0ea366d94e10fefad
Emmanuel Gautier <git@emmanuelgautier.fr>
2024-08-31 11:15:58 +0200

    chore: add webapp languages templates

--- templates/index.ts
+++ templates/index.ts
@@ -24,6 +24,8 @@ export type Template = {
 import angularSpa from './angular-spa.json' assert { type: 'json' }
 import astroApp from './astro-app.json' assert { type: 'json' }
 import codeigniterApp from './codeigniter-app.json' assert { type: 'json' }
+import dotnetWebApp from './dotnet-web-app.json' assert { type: 'json' }
+import elixirApp from './elixir-app.json' assert { type: 'json' }
 import expressJsApp from './expressjs-app.json' assert { type: 'json' }
 import flutterWebSpa from './flutter-web-spa.json' assert { type: 'json' }
 import golangApp from './golang-app.json' assert { type: 'json' }
@@ -32,16 +34,22 @@ import golangEchoApp from './golang-echo-app.json' assert { type: 'json' }
 import golangFiberApp from './golang-fiber-app.json' assert { type: 'json' }
 import golangGinApp from './golang-gin-app.json' assert { type: 'json' }
 import hapiApp from './hapi-app.json' assert { type: 'json' }
+import haskellApp from './haskell-app.json' assert { type: 'json' }
+import javaWebApp from './java-web-app.json' assert { type: 'json' }
+import kotlinWebApp from './kotlin-web-app.json' assert { type: 'json' }
 import laravelApp from './laravel-app.json' assert { type: 'json' }
 import meteorJsApp from './meteorjs-app.json' assert { type: 'json' }
 import nestJsApp from './nestjs-app.json' assert { type: 'json' }
 import nextjsApp from './nextjs-app.json' assert { type: 'json' }
 import phpApp from './php-app.json' assert { type: 'json' }
 import preactSpa from './preact-spa.json' assert { type: 'json' }
+import pythonApp from './python-app.json' assert { type: 'json' }
 import reactSpa from './react-spa.json' assert { type: 'json' }
 import remixApp from './remix-app.json' assert { type: 'json' }
+import rubyApp from './ruby-app.json' assert { type: 'json' }
 import rustApp from './rust-app.json' assert { type: 'json' }
 import sailsJsApp from './sailsjs-app.json' assert { type: 'json' }
+import scalaApp from './scala-app.json' assert { type: 'json' }
 import spa from './spa.json' assert { type: 'json' }
 import svelteSpa from './svelte-spa.json' assert { type: 'json' }
 import symfonyApp from './symfony-app.json' assert { type: 'json' }
@@ -54,6 +62,8 @@ export {
   angularSpa,
   astroApp,
   codeigniterApp,
+  dotnetWebApp,
+  elixirApp,
   expressJsApp,
   flutterWebSpa,
   golangApp,
@@ -62,16 +72,22 @@ export {
   golangFiberApp,
   golangGinApp,
   hapiApp,
+  haskellApp,
+  javaWebApp,
+  kotlinWebApp,
   laravelApp,
   meteorJsApp,
   nestJsApp,
   nextjsApp,
   phpApp,
   preactSpa,
+  pythonApp,
   reactSpa,
   remixApp,
+  rubyApp,
   rustApp,
   sailsJsApp,
+  scalaApp,
   spa,
   svelteSpa,
   symfonyApp,
@@ -85,6 +101,8 @@ export const templates = [
   angularSpa,
   astroApp,
   codeigniterApp,
+  dotnetWebApp,
+  elixirApp,
   expressJsApp,
   flutterWebSpa,
   golangApp,
@@ -93,16 +111,22 @@ export const templates = [
   golangFiberApp,
   golangGinApp,
   hapiApp,
+  haskellApp,
+  javaWebApp,
+  kotlinWebApp,
   laravelApp,
   meteorJsApp,
   nestJsApp,
   nextjsApp,
   phpApp,
   preactSpa,
+  pythonApp,
   reactSpa,
   remixApp,
+  rubyApp,
   rustApp,
   sailsJsApp,
+  scalaApp,
   spa,
   svelteSpa,
   symfonyApp,
