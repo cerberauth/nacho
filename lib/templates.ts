a9bfbb1e81393154ede252a995c7fe2af62351e3
Emmanuel Gautier <git@emmanuelgautier.fr>
2024-08-31 11:52:13 +0200

    feat: improve related templates

--- lib/templates.ts
+++ lib/templates.ts
@@ -3,3 +3,15 @@ import { templates } from '@/templates'
 export const getTemplateById = (id: string) => {
   return templates.find((t) => t.identifier === id)
 }
+
+export const getRelatedTemplates = (id: string) => {
+  const template = getTemplateById(id)
+  if (!template) {
+    return []
+  }
+
+  return templates.filter((t) =>
+    t.client.applicationType === template.client.applicationType &&
+    t.technologies.some((tag) => template.technologies.includes(tag))
+  )
+}
