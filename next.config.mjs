6967578802a33f53e348618fb43125aba0203705
Emmanuel Gautier <git@emmanuelgautier.fr>
2024-09-29 15:42:33 +0200

    feat: add security headers

--- next.config.mjs
+++ next.config.mjs
@@ -7,10 +7,91 @@ if (process.env.NODE_ENV === 'development') {
   await setupDevPlatform()
 }
 
+const cspHeader = `
+    default-src 'self';
+    connect-src 'self' https://a.cerberauth.com;
+    script-src 'self' https://a.cerberauth.com/js/plausible.outbound-links.tagged-events.js;
+    style-src 'self';
+    img-src 'self';
+    frame-src 'none';
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['nacho.cerberauth.com'],
+  },
+  headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://nacho.cerberauth.com',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, OPTIONS',
+          },
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'fullscreen=(self)',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
+      {
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://nacho.cerberauth.com',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Credentials',
+            value: 'true',
+          },
+        ],
+      },
+    ]
   },
   async redirects() {
     return [
